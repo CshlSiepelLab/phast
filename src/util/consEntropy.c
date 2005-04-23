bfdab8ad9e96deeaedec4917f178569b8807b2aa
Adam Siepel <asiepel@cshl.edu>
2005-04-23 19:07:29 +0000

    added L_max computation, changed some names for consistency with paper, added check for infinite loop in newton search
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@339 3e584e52-61a1-4911-b546-322c73959ec4

--- src/util/consEntropy.c
+++ src/util/consEntropy.c
@@ -14,10 +14,11 @@ DESCRIPTION:\n\
     For use with phastCons.  Given phylogenetic models for conserved\n\
     and non-conserved states, the target coverage, and the (prior)\n\
     expected length of a conserved element, compute the relative\n\
-    entropy (H) of the phylogenetic models and the expected number (N)\n\
-    of conserved sites required to predict conserved element.\n\
-    Also will make a recommendation for a new prior expected length\n\
-    based on a constant value of NH (see --NH).\n\
+    entropy (H) of the phylogenetic models, the expected minimum number\n\
+    of conserved sites required to predict conserved element (L_min), and\n\
+    the expected maximum number of nonconserved sites tolerated within a\n\
+    conserved element (L_max).  Also will make a recommendation for a\n\
+    new prior expected length based on a constant value of L_min*H (see --NH).\n\
 \n\
 USAGE: %s [OPTIONS] <target-coverage> <expected-length> \\\n\
             [ <cons.mod> <noncons.mod> ]\n\
@@ -28,10 +29,10 @@ OPTIONS:\n\
         just use the specified value.  The .mod files aren't required\n\
         in this case.\n\
 \n\
-    --NH, -N <value>\n\
+    --LminH, -L <value> [or --NH/-N, for backward compatibility]\n\
         Report the expected length that would produce the specified\n\
-        value of NH, assuming H remains constant (it generally won't).\n\
-        Can be used iteratively to converge on a desired value of NH.\n\
+        value of L_min*H, assuming H remains constant (it generally won't).\n\
+        Can be used iteratively to converge on a desired value of L_min*H.\n\
 \n\
     --help, -h\n\
         Print this help message.\n\
@@ -43,24 +44,27 @@ NOTE:\n\
   exit(0);
 }
 
-/* solve for new expected length given NH using Newton's method */
-double solve_newton(double expected_len, double target_coverage, double H, double NH) {
-  double N, odds, mu1, mu2, func, deriv;
-  H = H * log(2);               /* switch to natural log scale -- makes derivatives simpler */
-  NH = NH * log(2);
-  N = NH/H;
+/* solve for new expected length given L_min*H using Newton's method */
+double solve_newton(double expected_len, double target_coverage, double H, double LminH) {
+  double L_min, odds, mu1, mu2, func, deriv;
+  int niter = 0;
+  H = H * log(2); /* switch to natural log scale -- makes derivatives simpler */
+  LminH = LminH * log(2);
+  L_min = LminH/H;
   odds = target_coverage / (1-target_coverage);
   mu1 = 1/expected_len;
   fprintf(stderr, "\n( Solving for new omega: %f ", 1/mu1);
   while (TRUE) {
-    func = (N+1) * log(1-odds*mu1) - (N-1) * log(1-mu1) - log(odds*mu1) - log(mu1) - NH;
-    deriv = -(N+1) * odds / (1 - odds * mu1) + (N-1) / (1-mu1) - 2/mu1;
+    func = (L_min+1) * log(1-odds*mu1) - (L_min-1) * log(1-mu1) - log(odds*mu1) - log(mu1) - LminH;
+    deriv = -(L_min+1) * odds / (1 - odds * mu1) + (L_min-1) / (1-mu1) - 2/mu1;
     mu2 = mu1 - func/deriv;
     if (mu2 < 0) mu2 = 1e-3;
     else if (mu2 > 1) mu2 = 1 - 1e-3;
     fprintf(stderr, "%f ", 1/mu2);
     if (fabs(mu2-mu1) < 1e-4) break;
     mu1 = mu2;
+    niter++;
+    if (niter >= 30) die("ERROR: too many iterations, not converging; try without --NH.");
   }
   fprintf(stderr, ")\n\n");
   return 1/mu2;
@@ -73,11 +77,12 @@ int main(int argc, char *argv[]) {
   MSA *msa;
   double *cons_lprob, *noncons_lprob;
   char *leaf_labels;
-  double H = -1, checksum1, checksum2, target_coverage, expected_len, 
-    mu, nu, N, new_exp_len, NH = -1;
+  double H = -1, H_alt = -1, checksum1, checksum2, target_coverage, 
+    expected_len, mu, nu, L_min, L_max, new_exp_len, LminH = -1;
 
   struct option long_opts[] = {
-    {"NH", 1, 0, 'N'},
+    {"LminH", 1, 0, 'L'},
+    {"NH", 1, 0, 'N'},		/* backward compatibility */
     {"H", 1, 0, 'H'},
     {"help", 0, 0, 'h'},
     {0, 0, 0, 0}
@@ -88,8 +93,9 @@ int main(int argc, char *argv[]) {
     case 'H':
       H = get_arg_dbl_bounds(optarg, 0, INFTY);
       break;
-    case 'N':
-      NH = get_arg_dbl_bounds(optarg, 0, INFTY);
+    case 'N':			/* backward compatibility */
+    case 'L':
+      LminH = get_arg_dbl_bounds(optarg, 0, INFTY);
       break;
     case 'h':
       usage(argv[0]);
@@ -138,13 +144,17 @@ int main(int argc, char *argv[]) {
     tl_compute_log_likelihood(cons_mod, msa, cons_lprob, -1, NULL);
     tl_compute_log_likelihood(noncons_mod, msa, noncons_lprob, -1, NULL);
 
-    H = 0;
+    H = H_alt = 0;		/* H is relative entropy of cons wrt
+				   noncons; H_alt is relative entropy
+				   of noncons wrt cons */
     checksum1 = checksum2 = 0;
     for (i = 0; i < nlabels; i++) {
       double tmp = exp2(cons_lprob[i]); /* tl_compute_log_likelihood uses base 2 */
+      double tmp2 = exp2(noncons_lprob[i]);
       checksum1 += tmp;
-      checksum2 += exp2(noncons_lprob[i]);
+      checksum2 += tmp2;
       H += tmp * (cons_lprob[i] - noncons_lprob[i]);
+      H_alt += tmp2 * (noncons_lprob[i] - cons_lprob[i]);
     }
 
     if (fabs(checksum1 - 1) > 1e-4 || fabs(checksum1 - 1) > 1e-4)
@@ -153,17 +163,19 @@ int main(int argc, char *argv[]) {
 
   mu = 1/expected_len;
   nu = mu * target_coverage / (1-target_coverage);
-  N = (log2(nu) + log2(mu) - log2(1-nu) - log2(1-mu)) / (log2(1-nu) - log2(1-mu) - H);
+  L_min = (log2(nu) + log2(mu) - log2(1-nu) - log2(1-mu)) / (log2(1-nu) - log2(1-mu) - H);
+  L_max = (log2(nu) + log2(mu) - log2(1-nu) - log2(1-mu)) / (log2(1-mu) - log2(1-nu) - H_alt);
 
-  if (NH > -1) 
-    new_exp_len = solve_newton(expected_len, target_coverage, H, NH);
+  if (LminH > -1) 
+    new_exp_len = solve_newton(expected_len, target_coverage, H, LminH);
 
   printf("Transition parameters: gamma=%f, omega=%f, mu=%f, nu=%f\n", 
          target_coverage, expected_len, mu, nu);
   printf("Relative entropy: H=%f bits/site\n", H);
-  printf("Required length: N=%f sites\n", N);
-  printf("Total entropy: NH=%f bits\n", N*H);
-  if (NH > -1)
-    printf("Recommended expected length: omega=%f sites (for NH=%f)\n", new_exp_len, NH);
+  printf("Expected min. length: L_min=%f sites\n", L_min);
+  printf("Expected max. length: L_max=%f sites\n", L_max);
+  printf("Total entropy: L_min*H=%f bits\n", L_min*H);
+  if (LminH > -1)
+    printf("Recommended expected length: omega=%f sites (for L_min*H=%f)\n", new_exp_len, LminH);
   return 0;
 }
