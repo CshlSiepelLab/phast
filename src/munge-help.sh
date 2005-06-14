b5dc155159a70eb5ddfb557239a9f6c6cb7eb36b
Adam Siepel <asiepel@cshl.edu>
2005-06-14 03:51:57 +0000

    added new backslash rule
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@367 3e584e52-61a1-4911-b546-322c73959ec4

--- src/munge-help.sh
+++ src/munge-help.sh
@@ -3,6 +3,6 @@
 # preprocess help files for inclusion in executables
 
 # let's just assume sed is in path
-sed 's/$/\\n\\/ ; s/"/\\"/g ; s/%/%%/g ; 1s/^/char* HELP = "\\n\\n/ ; $s/$/\n";/' $1
+sed 's/\\$/\\\\/ ; s/$/\\n\\/ ; s/"/\\"/g ; s/%/%%/g ; 1s/^/char* HELP = "\\n/ ; $s/$/\n";/' $1
 
 
