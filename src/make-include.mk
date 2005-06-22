79a259ed6fbc8a4681fb10b01b04223b5e05a86d
Adam Siepel <asiepel@cshl.edu>
2005-06-22 07:17:10 +0000

    removed GSL stuff, updated comments, etc.
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@381 3e584e52-61a1-4911-b546-322c73959ec4

--- src/make-include.mk
+++ src/make-include.mk
@@ -21,28 +21,22 @@ BIN = ${PHAST}/bin
 TARGETLIB = ${LIB}/libphast.a
 
 # for debugging
-CFLAGS = -g -fno-inline -Wall -DGCC -Wno-long-double
+#CFLAGS = -g -fno-inline -Wall -DGCC -Wno-long-double
 # for best performance
-#CFLAGS = -mcpu=pentiumpro -O3 -DGCC -DGSL_RANGE_CHECK_OFF
+CFLAGS = -mcpu=pentiumpro -O3 -DGCC
 # use this instead for Mac OS X
-#CFLAGS = -mcpu=powerpc -O3 -DGCC -DGSL_RANGE_CHECK_OFF -Wno-long-double
+#CFLAGS = -mcpu=powerpc -O3 -DGCC -Wno-long-double
 # possible x86-64 options (kolossus at UC Santa Cruz)
-#CFLAGS = -mcpu=opteron -O3 -DGCC -DGSL_RANGE_CHECK_OFF
+#CFLAGS = -mcpu=opteron -O3 -DGCC
 # possible profiling options
-#CFLAGS = -mcpu=pentiumpro -O3 -DGCC -pg -fprofile-arcs -DGSL_RANGE_CHECK_OFF
+#CFLAGS = -mcpu=pentiumpro -O3 -DGCC -pg -fprofile-arcs
 # NOTE: add -g if profiling line-by-line, and -a if monitoring basic blocks
 
 CFLAGS += -DPHAST_VERSION=\"$(shell cat ${PHAST}/version)\" -DPHAST_HOME=\"${PHAST}\"
 
 # static linking ends up being simplest in our environment; comment
 # this line out to link dynamically (comment out for Mac)
-#LFLAGS = -static
-
-# define if GSL files not in standard locations (such as /usr/include/gsl
-# or /usr/lib)
-#GSLPATH = /projects/compbio/usr/acs/gsl-1.4-P3
-#GSLPATH = /Users/acs/UTILITIES/gsl-1.6
-# above to be removed
+LFLAGS = -static
 
 # Uncomment this line to compile without CLAPACK.  If SKIP_CLAPACK is
 # defined, PHAST will compile, but programs that require matrix
@@ -50,25 +44,17 @@ CFLAGS += -DPHAST_VERSION=\"$(shell cat ${PHAST}/version)\" -DPHAST_HOME=\"${PHA
 #SKIP_CLAPACK = T
 
 # must be defined unless SKIP_CLAPACK (see Makefile in lib subdir)
-#CLAPACKPATH = /projects/compbio/usr/acs/CLAPACK
-CLAPACKPATH = /Users/acs/UTILITIES/CLAPACK
+CLAPACKPATH = /projects/compbio/usr/acs/CLAPACK
 
 # define if no SKIP_CLAPACK and F2C files not in standard locations
 F2CPATH = ${CLAPACKPATH}/F2CLIBS
 
 # architecture-specific suffix used for CLAPACK libraries; acceptable
 # values include LINUX, SUN4SOL2, HPPA, ALPHA, OCTANE, RS6K, MACOSX
-#ARCH=LINUX
-ARCH=MACOSX
+ARCH=LINUX
 
 CFLAGS += -I${INC}
 LIBPATH = -L${LIB} 
-
-# to be removed
-#ifdef GSLPATH
-#CFLAGS += -I${GSLPATH}/include
-#LIBPATH += -L${GSLPATH}/lib
-#endif
 
 ifndef SKIP_CLAPACK
 CFLAGS += -I${CLAPACKPATH}
@@ -76,16 +62,11 @@ ifdef F2CPATH
 CFLAGS += -I${F2CPATH}
 LIBPATH += -L${F2CPATH} 
 endif
-LIBS =  -lphast -llapack -ltmg -lblaswr -lc \
-	-lF77 -lI77 -lm
-#LIBS =  -lphast -lgsl -llapack -ltmg -lblaswr -lc \
-#	-lF77 -lI77 -lgslcblas -lm
+LIBS = -lphast -llapack -ltmg -lblaswr -lc -lF77 -lI77 -lm
 else
 CFLAGS += -DSKIP_CLAPACK
-LIBS =  -lphast -lc -lm
+LIBS = -lphast -lm
 endif
-
-# remove gsl and gslcblas above; is -lc necessary?
 
 # NOTE: currently assuming version of BLAS that comes with CLAPACK;
 # might want to allow alternatives.
