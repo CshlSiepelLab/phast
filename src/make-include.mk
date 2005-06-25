1406c76a780a73e29a897a2d2fde3987c5c1801d
Adam Siepel <asiepel@cshl.edu>
2005-06-25 05:05:03 +0000

    simplified and clarified a bit more
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@395 3e584e52-61a1-4911-b546-322c73959ec4

--- src/make-include.mk
+++ src/make-include.mk
@@ -3,36 +3,43 @@
 ###########################################################################
 
 # currently set up for linux and GCC and Pentium III/IV processors,
-# with options for Mac OS X.  Adjustments will be needed for other
+# with options for Mac OS X.  Adjustments may be needed for other
 # systems.
 
-# set either below or environment variable PHAST to point to top-level directory
+# set below to point to top-level directory of PHAST installation
 ifndef PHAST
 PHAST=${HOME}/phast
 endif
+# (if you prefer, you can set the environment variable PHAST instead)
 
+# specify alternative compiler or utilities if necessary
 CC = gcc
 AR = ar
 LN = ln
+
 LIB = ${PHAST}/lib
 INC = ${PHAST}/include
 BIN = ${PHAST}/bin
 
 TARGETLIB = ${LIB}/libphast.a
 
+# set compiler options; uncomment one of the lines below or define
+# an appropriate alternative
+
 # for debugging
-#CFLAGS = -g -fno-inline -Wall -DGCC -Wno-long-double
+#CFLAGS = -g -fno-inline -Wall -Wno-long-double
 # for best performance (pentiumpro)
-CFLAGS = -mcpu=pentiumpro -O3 -DGCC
+CFLAGS = -mcpu=pentiumpro -O3 
 # use this instead for Mac OS X
-#CFLAGS = -mcpu=powerpc -O3 -DGCC -Wno-long-double
+#CFLAGS = -mcpu=powerpc -O3 -Wno-long-double
 # possible x86-64 options (kolossus at UC Santa Cruz)
-#CFLAGS = -mcpu=opteron -O3 -DGCC
+#CFLAGS = -mcpu=opteron -O3
 # possible profiling options
-#CFLAGS = -mcpu=pentiumpro -O3 -DGCC -pg -fprofile-arcs
+#CFLAGS = -mcpu=pentiumpro -O3 -pg -fprofile-arcs
 # NOTE: add -g if profiling line-by-line, and -a if monitoring basic blocks
 
-CFLAGS += -DPHAST_VERSION=\"$(shell cat ${PHAST}/version)\" -DPHAST_HOME=\"${PHAST}\"
+CFLAGS += -I${INC} -DPHAST_VERSION=\"$(shell cat ${PHAST}/version)\" -DPHAST_HOME=\"${PHAST}\"
+LIBPATH = -L${LIB} 
 
 # static linking ends up being simplest in our environment; comment
 # this line out to link dynamically (comment out for Mac)
@@ -46,23 +53,17 @@ LFLAGS = -static
 # root directory for CLAPACK; necessary unless SKIP_CLAPACK is defined
 CLAPACKPATH = /projects/compbio/usr/acs/CLAPACK
 
-# location of F2C libraries used by CLAPACK; you shouldn't need to
-# edit this unless you're using a different version of F2C
+# location of F2C libraries used by CLAPACK; most people won't need to
+# edit this
 F2CPATH = ${CLAPACKPATH}/F2CLIBS
 
 # platform-specific suffix used for CLAPACK libraries; use the same
 # value used in the "make.inc" file when compiling CLAPACK
 PLAT = _x86
 
-CFLAGS += -I${INC}
-LIBPATH = -L${LIB} 
-
 ifndef SKIP_CLAPACK
-CFLAGS += -I${CLAPACKPATH}
-ifdef F2CPATH
-CFLAGS += -I${F2CPATH}
+CFLAGS += -I${CLAPACKPATH} -I${F2CPATH}
 LIBPATH += -L${F2CPATH} 
-endif
 LIBS = -lphast -llapack -ltmg -lblaswr -lc -lF77 -lI77 -lm
 else
 CFLAGS += -DSKIP_CLAPACK
