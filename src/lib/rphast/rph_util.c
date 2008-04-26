347a100500175fd763793152a934b0e464bcf6ac
ajd45 <ajd45>
2008-04-26 18:01:56 +0000

    Utility functions for RPHAST
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@1467 3e584e52-61a1-4911-b546-322c73959ec4

---
+++ src/lib/rphast/rph_util.c
@@ -0,0 +1,28 @@
+/****************************************************
+rph_util.c
+RPHAST low-level utility functions.
+These functions should not be needed by the user.
+
+Alexandra Denby
+Last updated 4/26/08
+****************************************************/
+#include <rph_util.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int rphast_errno=0;
+char rphast_errmsg[1000];
+
+void init(){
+  /*rphast_errno=0;
+    rphast_errmsg=(char*)malloc(sizeof(char));*/
+}
+
+void* ad2ptr(double address){
+  return((void*)(unsigned long)address);
+}
+
+double ptr2ad(void* ptr){
+  return((double)(unsigned long)ptr);
+}
+
