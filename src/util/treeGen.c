b778eaa119369e2fb122d192fd87eae7f6bb90ac
Adam Siepel <asiepel@cshl.edu>
2009-07-30 14:17:49 +0000

    New program to enumerate tree topologies
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@2166 3e584e52-61a1-4911-b546-322c73959ec4

---
+++ src/util/treeGen.c
@@ -0,0 +1,201 @@
+/***************************************************************************
+ * PHAST: PHylogenetic Analysis with Space/Time models
+ * Copyright (c) 2002-2005 University of California, 2006-2009 Cornell 
+ * University.  All rights reserved.
+ *
+ * This source code is distributed under a BSD-style license.  See the
+ * file LICENSE.txt for details.
+ ***************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <misc.h>
+#include <trees.h>
+#include <assert.h>
+#include "treeGen.help"
+
+int num_rooted_topologies(int n);
+TreeNode *tr_new_trivial(char *name1, char *name2);
+void tr_add_leaf_internal(TreeNode *t, int branch, char *lname);
+void tr_add_leaf_at_root(TreeNode *t, char *lname);
+
+
+int main(int argc, char *argv[]) {
+  char c;
+  int opt_idx, i, j, k, N, nleaves;
+  List *names, *treelist, *newlist, *tmpl;
+  TreeNode *t, *tnew;
+
+  struct option long_opts[] = {
+    {"help", 0, 0, 'h'},
+    {0, 0, 0, 0}
+  };
+
+  while ((c = getopt_long(argc, argv, "h", long_opts, &opt_idx)) != -1) {
+    switch (c) {
+    case 'h':
+      printf(HELP);
+      exit(0);
+    case '?':
+      die("Bad argument.  Try 'treeGen -h'.\n");
+    }
+  }
+
+  if (optind != argc - 1) 
+    die("ERROR: Wrong number of arguments.  Try 'treeGen -h'.\n");
+
+  names = get_arg_list(argv[optind]);
+
+  if (lst_size(names) <= 1)
+    die("ERROR: must specify at least two species names.\n");
+
+  nleaves = lst_size(names) - 1; /* excluding outgroup */
+
+  N = num_rooted_topologies(nleaves); 
+
+  /* FIXME: eventually need to consider constraints here */
+
+  if (N > 1e9)
+    fprintf(stderr, "WARNING: very large number of topologies expected (%d).  Program may not finish.\n", N);
+
+  /* start with tree consisting of first two names */
+  t = tr_new_trivial(((String*)lst_get_ptr(names, 0))->chars, 
+                     ((String*)lst_get_ptr(names, 1))->chars);
+
+  treelist = lst_new_ptr(1000);
+  newlist = lst_new_ptr(1000);
+  lst_push_ptr(treelist, t);
+
+  for (i = 2; i < nleaves; i++) {
+    char *nextname = ((String*)lst_get_ptr(names, i))->chars;
+    lst_clear(newlist);
+
+    for (j = 0; j < lst_size(treelist); j++) {
+      t = lst_get_ptr(treelist, j);
+
+      /* create copies and add leaf to each internal branch */
+      for (k = 1; k < t->nnodes; k++) {
+        tnew = tr_create_copy(t);
+        tr_add_leaf_internal(tnew, k, nextname);
+        lst_push_ptr(newlist, tnew);
+      }
+
+      /* now add leaf at root; this time reuse the original copy to
+         avoid unnecessary memory reallocation */
+      tr_add_leaf_at_root(t, nextname);
+      lst_push_ptr(newlist, t);
+    }
+
+    /* swap treelist and newlist */
+    tmpl = treelist;
+    treelist = newlist;
+    newlist = tmpl;
+  }
+
+  /* traverse list and add outgroup at root of each tree */
+  for (j = 0; j < lst_size(treelist); j++) {
+    t = lst_get_ptr(treelist, j);
+    tr_add_leaf_at_root(t, ((String*)lst_get_ptr(names, nleaves))->chars);
+  }
+
+  /* print trees */
+  for (j = 0; j < lst_size(treelist); j++) {
+    t = lst_get_ptr(treelist, j);
+    tr_print(stdout, t, FALSE);
+  }
+
+  return 0;
+}
+
+/* compute number of rooted topologies for n species: (2n-3)!! */
+int num_rooted_topologies(int n) {
+  int multiplier = 2*n - 3, retval = 1;
+  while (multiplier > 1) {
+    retval *= multiplier;
+    multiplier -= 2;
+  }
+  return retval;
+}
+
+/* create a trivial, two-leaf tree */
+TreeNode *tr_new_trivial(char *name1, char *name2) {
+  TreeNode *root;
+  root = tr_new_node();
+  root->lchild = tr_new_node();
+  strcpy(root->lchild->name, name1);
+  root->lchild->parent = root;
+  root->rchild = tr_new_node();
+  strcpy(root->rchild->name, name2);
+  root->rchild->parent = root;
+
+  /* bypass default handling of ids and nodes list */
+  root->nnodes = 3;
+  root->id = 0;
+  root->lchild->id = 1;
+  root->rchild->id = 2;  
+  root->nodes = lst_new_ptr(root->nnodes);
+  lst_push_ptr(root->nodes, root);
+  lst_push_ptr(root->nodes, root->lchild);
+  lst_push_ptr(root->nodes, root->rchild);
+
+  return root;
+}
+
+/* add leaf with specified name to specified internal branch */
+void tr_add_leaf_internal(TreeNode *t, int branch, char *lname) {
+  TreeNode *oldnode, *newanc, *newleaf;
+
+  oldnode = lst_get_ptr(t->nodes, branch); /* node beneath branch in question */
+  assert(oldnode != t);
+
+  newanc = tr_new_node();
+  newleaf = tr_new_node();
+  strcpy(newleaf->name, lname);
+
+  newanc->rchild = newleaf;
+  newleaf->parent = newanc;
+  newanc->lchild = oldnode;
+  newanc->parent = oldnode->parent; 
+
+  if (oldnode->parent->lchild == oldnode)
+    oldnode->parent->lchild = newanc;
+  else 
+    oldnode->parent->rchild = newanc;
+
+  oldnode->parent = newanc;
+
+  /* fix up ids and nodes list */
+  lst_push_ptr(t->nodes, newanc);
+  newanc->id = lst_size(t->nodes) - 1; /* circumvent normal id assignment */
+  lst_push_ptr(t->nodes, newleaf);
+  newleaf->id = lst_size(t->nodes) - 1;
+  t->nnodes += 2;
+}
+
+/* add a leaf with specified name to root branch */
+void tr_add_leaf_at_root(TreeNode *t, char *lname) {
+  TreeNode *newanc, *newleaf;
+
+  newanc = tr_new_node();
+  newleaf = tr_new_node();
+  strcpy(newleaf->name, lname);
+
+  /* we don't want to change the identity of the root node, so will
+     add the new node below it and rewire as necessary */
+  newanc->lchild = t->lchild;
+  newanc->rchild = t->rchild;
+  t->lchild->parent = newanc;
+  t->rchild->parent = newanc;
+  t->lchild = newanc;
+  t->rchild = newleaf;
+  newanc->parent = t;
+  newleaf->parent = t;
+
+  /* fix up ids and nodes list */
+  lst_push_ptr(t->nodes, newanc);
+  newanc->id = lst_size(t->nodes) - 1; /* circumvent normal id assignment */
+  lst_push_ptr(t->nodes, newleaf);
+  newleaf->id = lst_size(t->nodes) - 1;
+  t->nnodes += 2;
+}
