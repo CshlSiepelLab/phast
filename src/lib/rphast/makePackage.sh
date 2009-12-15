62f204f645a9b15b13d2b78c85b1254eb9c33952
mt269 <mt269>
2009-12-15 19:19:41 +0000

    fixed one more problem (wasn't linking to clapack)
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@2597 3e584e52-61a1-4911-b546-322c73959ec4

--- src/lib/rphast/makePackage.sh
+++ src/lib/rphast/makePackage.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 PHAST_VERSION=$1
-CLAPACK_INCLUDE=$2/INCLUDE
-RPHAST=$3
+CLAPACK=$2
+PLAT=$3
+RPHAST=$4
 
 mkdir -p RPHAST/src/include
 cp -ua `find ../ -name "*.c" | grep -v "RPHAST/src"` RPHAST/src
@@ -15,7 +16,7 @@ echo -e \
 "include ../../../../make-include.mk
 OBJECTS = \$(addsuffix .o, \$(basename \$(wildcard *.c)))
 all: \$(OBJECTS)
-\t\$(CC) -shared -o rphast.so \$(CFLAGS) \$(LIBPATH) \$(LFLAGS) -L/usr/lib64/R/lib -lR \$(OBJECTS)
+\t\$(CC) -shared -o rphast.so \$(CFLAGS) \$(LIBPATH) \$(LFLAGS) -L/usr/lib64/R/lib -lR \$(OBJECTS) -I$CLAPACK/INCLUDE $CLAPACK/lapack_$PLAT.a $CLAPACK/blas_$PLAT.a $CLAPACK/F2CLIBS/libf2c.a
 
 .o: %.c %.h
 \t\$(CC) \$(CFLAGS) -c \$< -o \$@
@@ -24,7 +25,7 @@ clean:
 \trm -f *.o *.so" > RPHAST/src/Makefile
 else
 cat <<EOF > RPHAST/src/Makevars
-PKG_CFLAGS = -Iinclude/ -fgnu89-inline -DPHAST_VERSION=\"$PHAST_VERSION\" -DRPHAST -I$CLAPACK_INCLUDE
+PKG_CFLAGS = -Iinclude/ -fgnu89-inline -DPHAST_VERSION=\"$PHAST_VERSION\" -DRPHAST -I$CLAPACK/INCLUDE
 PKG_LIBS = \$(LAPACK_LIBS) \$(BLAS_LIBS) \$(FLIBS) -pthread
 CSOURCES = `echo $CSOURCES`
 OBJECTS = \$(CSOURCES:.c=.o)
