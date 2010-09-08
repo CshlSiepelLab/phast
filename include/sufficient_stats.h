1fdab43b5660a14c4a1e46c525f7a0e68821cc37
nrp36 <nrp36>
2010-09-08 19:14:16 +0000

    Updated Copyright from 2006-2009 Cornell to 2006-2010 Cornell
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@3302 3e584e52-61a1-4911-b546-322c73959ec4

--- include/sufficient_stats.h
+++ include/sufficient_stats.h
@@ -1,7 +1,7 @@
 
 /***************************************************************************
  * PHAST: PHylogenetic Analysis with Space/Time models
- * Copyright (c) 2002-2005 University of California, 2006-2009 Cornell 
+ * Copyright (c) 2002-2005 University of California, 2006-2010 Cornell 
  * University.  All rights reserved.
  *
  * This source code is distributed under a BSD-style license.  See the
