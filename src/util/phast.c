70d94ee4d865c96d0b63501e16638acd961c052f
nrp36 <nrp36>
2010-10-19 22:55:36 +0000

    Added default phast program for new users
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@3458 3e584e52-61a1-4911-b546-322c73959ec4

---
+++ src/util/phast.c
@@ -0,0 +1,17 @@
+/***************************************************************************
+ * PHAST: PHylogenetic Analysis with Space/Time models
+ * Copyright (c) 2002-2005 University of California, 2006-2010 Cornell 
+ * University.  All rights reserved.
+ *
+ * This source code is distributed under a BSD-style license.  See the
+ * file LICENSE.txt for details.
+ ***************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "phast.help"
+
+int main(int argc, char *argv[]) {
+  printf("%s", HELP);
+  return 0;
+}
