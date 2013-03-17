6f5e982e3336016d7aaa1f9c28a3ecf34e365f15
mt269 <mt269>
2013-03-17 18:43:40 +0000

    added ability to add comments to help-src files as lines beginning with #
    git-svn-id: svn+ssh://nextgen.cshl.edu/usr/local/svnroot/phast/trunk@6035 3e584e52-61a1-4911-b546-322c73959ec4

--- src/munge-help.sh
+++ src/munge-help.sh
@@ -2,7 +2,9 @@
 
 # preprocess help files for inclusion in executables
 
+# Note: any line in .help_src file beginning with a pound sign is discarded
+
 # let's just assume sed is in path
-sed 's/\\$/\\\\/ ; s/$/\\n\\/ ; s/"/\\"/g ; s/%/%%/g ; 1s/^/char* HELP = "\\n/ ; $s/$/\n";/' $1
+sed '/^#.*$/d ; s/\\$/\\\\/ ; s/$/\\n\\/ ; s/"/\\"/g ; s/%/%%/g ; 1s/^/char* HELP = "\\n/ ; $s/$/\n";/' $1
 
 
