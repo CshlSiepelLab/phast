bb6ff2d2df5d3900c87e6fea4ab3fcf874ae4c8d
Adam Siepel <asiepel@cshl.edu>
2025-10-03 12:21:11 -0400

    rolled back some experimental sampling routines

--- include/phast/var_resampling.h
+++ include/phast/var_resampling.h
@@ -19,27 +19,4 @@ List *nj_var_sample_importance(int nsamples, multi_MVN *mmvn,
                                CovarData *data, TreeModel *mod,
                                FILE *logf);
 
-List *nj_var_sample_importance_psis(int nsamples, multi_MVN *mmvn,
-                                    CovarData *data, TreeModel *mod,
-                                    FILE *logf);
-
-List *nj_var_sample_importance_moc(int nsamples, multi_MVN *mmvn,
-                                   CovarData *data, TreeModel *mod,
-                                   FILE *logf);
-
-List *nj_var_sample_smc(int nsamples, multi_MVN *mmvn,
-                        CovarData *data, TreeModel *mod,
-                        FILE *logf);
-
-List *nj_var_sample_pcn(int nsamples, multi_MVN *mmvn,
-                        CovarData *data, TreeModel *mod,
-                        FILE *logf /* unused; we print to stderr */);
-
-List *nj_var_sample_pcn_blocked(int nsamples, multi_MVN *mmvn,
-                               CovarData *data, TreeModel *mod,
-                               FILE *logf /* unused; diagnostics -> stderr */);
-
-List *nj_var_sample_pcn_pt(int nsamples, multi_MVN *mmvn,
-                           CovarData *data, TreeModel *mod,
-                           FILE *logf /* unused; diagnostics -> stderr */);
 #endif
--- src/util/vine.c
+++ src/util/vine.c
@@ -406,8 +406,7 @@ int main(int argc, char *argv[]) {
                          covar_data, logfile);
 
       if (rejection_sampling == TRUE) 
-        /* trees = nj_var_sample_rejection(nsamples, mmvn, covar_data, mod, logfile); */
-        trees = nj_var_sample_pcn_pt(nsamples, mmvn, covar_data, mod, logfile);
+        trees = nj_var_sample_rejection(nsamples, mmvn, covar_data, mod, logfile);
 
       else /* otherwise just sample directly from approx posterior */
         trees = nj_var_sample(nsamples, mmvn, covar_data, names, NULL);
