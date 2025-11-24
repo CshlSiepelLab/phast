86391b9448dd6d2f90e6d0f8549d29c42ef43208
Adam Siepel <asiepel@cshl.edu>
2025-11-23 20:37:26 -0500

    some final tweaks for more general LAPACK handling

--- include/phast/external_libs.h
+++ include/phast/external_libs.h
@@ -27,40 +27,38 @@
 #endif
 
 #ifdef R_LAPACK
-#include <R_ext/Lapack.h>
-#define LAPACK_INT int
-#define LAPACK_DOUBLE double
+   #include <R_ext/Lapack.h>
+   #define LAPACK_INT int
+   #define LAPACK_DOUBLE double
+
+#elif defined(VECLIB)
+   #include <Accelerate/Accelerate.h>
+   #ifdef ACCELERATE_NEW_LAPACK
+       /* New Accelerate LAPACK interface: use plain C types */
+       typedef int    LAPACK_INT;
+       typedef double LAPACK_DOUBLE;
+   #else
+     /* Old CLAPACK-style API (kept for older macOS) */
+     #define LAPACK_INT    __CLPK_integer
+     #define LAPACK_DOUBLE __CLPK_doublereal
+
+   #endif  /* ACCELERATE_NEW_LAPACK */
+
+#elif !defined(SKIP_LAPACK) && defined(PHAST_LAPACK_GENERIC)
+   /* Generic LAPACK: use C types and Fortran symbols (dgesv_, dsyev_, etc.) */
+   #define LAPACK_INT    int
+   #define LAPACK_DOUBLE double
+
+#elif !defined(SKIP_LAPACK)
+    /* Legacy CLAPACK + f2c path */
+    #include <f2c.h>
+    #include <clapack.h>
+    #define LAPACK_INT    integer
+    #define LAPACK_DOUBLE doublereal
+
 #else
+   /* SKIP_LAPACK defined: LAPACK is disabled */
 
-#ifdef VECLIB
-#include <Accelerate/Accelerate.h>
-#ifdef ACCELERATE_NEW_LAPACK
-
-   /* New Accelerate LAPACK interface: use plain C types */
-    typedef int    LAPACK_INT;
-    typedef double LAPACK_DOUBLE;
-
-#else
-
-  /* Old CLAPACK-style API (kept for older macOS / toolchains) */
-
-  /* New Accelerate LAPACK API */
-  #define LAPACK_INT    __CLPK_integer
-  #define LAPACK_DOUBLE __CLPK_doublereal
-
-#endif  /* ACCELERATE_NEW_LAPACK */
-
-#else /* VECLIB not defined */
-
-#ifndef SKIP_LAPACK 
-#include <f2c.h>  
-#include <clapack.h> 
-#define LAPACK_INT integer
-#define LAPACK_DOUBLE doublereal
-#endif  /*ifndef SKIP_LAPACK */
-
-#endif  /*ifdef VECLIB */
-
-#endif  /* ifdef R_LAPACK */
+#endif /* R_LAPACK, VECLIB, SKIP_LAPACK, etc. */
 
 #endif  /* ifndef PHAST_EXTERNAL_LIBS */
